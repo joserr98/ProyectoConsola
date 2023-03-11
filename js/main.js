101ab75854175e6bc11b01cb6c062a6a94cf3693
joserr98 <joserr9807@gmail.com>
2023-03-11 14:28:25 +0100

    Añadir más elementos a la pantalla de juego

--- js/main.js
+++ js/main.js
@@ -1,49 +1,71 @@
 const powerOn = document.querySelector(".powerOn");
 const powerOff = document.querySelector(".powerOff");
-const textScreen = document.querySelector(".screen-gameboy-text");
-const textScreen2 = document.querySelector(".screen-gameboy-text2");
+const superMarioText = document.querySelector(".supermario");
 const screen = document.querySelector("#screen");
+const mario = document.getElementById("mario")
+
+
 
 function encenderConsola() {
     let led = document.querySelector(".powerLed");
     led.classList.add('powerLedOn');
     led.classList.remove('powerLed');
-    
 
+
+    
+    // textScreen2.style.display = "block";
+    // textScreen2.style.display = "none";
 
     powerOn.style.display = "none";
     powerOff.style.display ="block";
     screen.style.backgroundColor = "white";
 
-    document.querySelector('.nintendo2').style.display = "block";
-    
-    setTimeout(nintendo, 1000);
-    setTimeout()
+ 
+    animationNintendo = setTimeout(nintendo, 0);
+    animationSuperMario = setTimeout(supermario, 1000);
+    animationGame = setTimeout(game,2250);
+
 }
 
 function nintendo() {
+    document.querySelector('.nintendo2').style.display = "block";
+
+
+}
+
+function supermario() {    
     document.querySelector('.nintendo2').style.display = "none";
-    textScreen.style.display = "block";
-    textScreen2.style.display = "block";
+    superMarioText.style.display = "block";
+    
+}
+
+function game() {
+    superMarioText.style.display = "none";
+    // GAME DISPLAY
 }
 
 function apagarConsola() {
+
     let led = document.querySelector(".powerLedOn")
     
     led.classList.remove('powerLedOn');
     led.classList.add('powerLedOff');
-    textScreen.style.display = "none";
-    textScreen2.style.display = "none";
-    screen.style.backgroundColor = "#747570";
+    superMarioText.style.display = "none";
+    
     powerOn.style.display = "block";
     powerOff.style.display = "none";
 
     setTimeout(redToInit,2000);
-
 }
 
 function redToInit() {
     let led = document.querySelector(".powerLedOff");
     led.classList.remove('powerLedOff');
     led.classList.add('powerLed');
+    screen.style.backgroundColor = "#747570";
+
+}
+
+function marioJump(){
+
 }
